43bb9ffea49ed6f5bfbbeeae1a67fb46539fe3e4
TiagoSantosdeJesus <tiagosantos2561@hotmail.com>
2023-01-09 11:37:44 -0300

    Tipos de variáveis

---
+++ Estudos/src/edu/tiago/tiposvariaveis/tiposvariaveis.java
@@ -0,0 +1,16 @@
+package edu.tiago.tiposvariaveis;
+
+import javax.print.DocFlavor.STRING;
+
+public class tiposvariaveis {
+    public static void main(String[] args) {
+        String nome = "Tiago";
+        byte idade = 26;
+        char sexo = 'M';
+        short ano = 2023;
+        int cep = 54170180;
+        long cpf = 12345678900L;
+        final float VALOR_DE_PI = 3.14F;
+        double salario = 2500.45;
+    }
+}
