e418f882ed2666b06417676f2340d75c01d537b4
TiagoSantosdeJesus <tiagosantos2561@hotmail.com>
2023-01-25 16:05:00 -0300

    Orientação a objetos

---
+++ Estudos/src/edu/tiago/oo/OperacoesMatematicas.java
@@ -0,0 +1,13 @@
+package edu.tiago.oo;
+
+// Exemplo com utilização de interface
+
+interface OperacoesMatematicas {
+    void soma(double num1, double num2);
+
+    void subtracao(double num1, double num2);
+
+    void multiplicacao(double num1, double num2);
+
+    void divisao(double num1, double num2);
+}
