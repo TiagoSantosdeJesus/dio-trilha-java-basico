45364145879791f3842b600f9d5c407f2dc315fd
TiagoSantosdeJesus <tiagosantos2561@hotmail.com>
2023-02-25 14:10:31 -0300

    Padrões de Projeto (Strategy)

--- Estudos/src/edu/tiago/padroesdeprojetos/strategy/ComportamentoDefensivo.java
+++ Estudos/src/edu/tiago/padroesdeprojetos/strategy/ComportamentoDefensivo.java
@@ -5,7 +5,6 @@ public class ComportamentoDefensivo implements Comportamento {
     @Override
     public void mover() {
         System.out.println("Movendo-se defensivamente...");
-        throw new UnsupportedOperationException("Unimplemented method 'mover'");
     }
 
 }
